ed8417e4087a6ca8c3eab6606f1d73c01de34883
UItimate <52617311+UItimate@users.noreply.github.com>
2022-07-11 03:47:12 -0600

    fix: check for empty password then reset styling

--- main.js
+++ main.js
@@ -1,16 +1,16 @@
 function onClick() {
     const password = document.querySelector('input[name=password]');
-    const password_confirm = document.querySelector('input[name=password_confirm]');
+    const passwordConfirm = document.querySelector('input[name=password_confirm]');
     const validPassword = document.querySelector('#validPassword');
 
-    if (password_confirm.value === password.value) {
-        password_confirm.setCustomValidity('');
+    if (password_confirm.value === password.value && passwordConfirm.value != '') {
+        password.setAttribute('style', 'padding-left: 5px; border-color: rgb(0 0 0 / 4 %); border: rgb(0 0 0 / 4 %); border-radius: 3px; margin-bottom: 20px');
+        passwordConfirm.setAttribute('style', 'padding-left: 5px; border-color: rgb(0 0 0 / 4 %); border: rgb(0 0 0 / 4 %); border-radius: 3px; margin-bottom: 20px');
         validPassword.textContent = ''
     } else {
-        password_confirm.setCustomValidity('Passwords do not match');
+        passwordConfirm.setAttribute('style', 'outline: none; border: 0.1px solid red;');
         password.setAttribute('style', 'outline: none; border: 0.1px solid red;');
-        password_confirm.setAttribute('style', 'outline: none; border: 0.1px solid red;');
-        validPassword.setAttribute('style', 'color: red; position: relative; margin-top: -22px;');
+        validPassword.setAttribute('style', 'color: red; position: absolute; margin-top: -22px;');
         validPassword.textContent = '* Passwords do not match'
     }
 }
